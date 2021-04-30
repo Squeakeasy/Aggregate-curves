2b5d6046998b4d235eca043ea59c0f4914ed2548
Eric Willisson <epw@google.com>
2021-04-29 20:10:10 -0400

    Changed lines to dots at one point per price

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const GRAPH_AXIS_SIZE = 180;
 const GRAPH_AXIS_OFFSET = 10.5;
+const POINT_R = 5;
+const PRICE_SCALE = 2;
 
 let graphs = [];
+
+if (typeof(Math.TAU) == "undefined") {
+    Math.TAU = Math.PI * 2;
+}
+
+function make_points() {
+    const points = [];
+    for (let y = 0; y <= 100; y += 10) {
+	points.push({x: 0, y: y});
+    }
+    return points;
+}
 
 class Graph {
     constructor(el) {
@@ -9,7 +23,7 @@ class Graph {
 	this.el.addEventListener("click", (e) => this.click(e));
 	this.axis_size = GRAPH_AXIS_SIZE;
 	this.axis_offset = GRAPH_AXIS_OFFSET;
-	this.points = [];
+	this.points = make_points();
     }
 
     draw() {
@@ -32,27 +46,33 @@ class Graph {
 
 	ctx.stroke();
 
-	if (this.points.length > 0) {
-	    ctx.beginPath();
-	    ctx.strokeStyle = "blue";
-	    ctx.moveTo(this.points[0].x, this.points[0].y);
-	    if (this.points.length == 1) {
-		ctx.lineTo(this.points[0].x, this.points[0].y);
-	    } else {
-		for (let point of this.points) {
-		    ctx.lineTo(point.x, point.y);
-		}
-	    }
-	    ctx.stroke();
-	}
+	this.draw_points(ctx);
 	
 	ctx.restore();
     }
+    draw_points(ctx) {
+	if (this.points.length == 0) {
+	    return;
+	}
+	ctx.beginPath();
+	ctx.strokeStyle = "blue";
+	ctx.moveTo(this.points[0].x, this.axis_size - this.points[0].y * PRICE_SCALE);
+	for (let point of this.points) {
+	    ctx.lineTo(point.x, this.axis_size - point.y * PRICE_SCALE);
+	}
+	ctx.stroke();
+
+	ctx.fillStyle = "blue";
+	for (let point of this.points) {
+	    ctx.beginPath();
+	    ctx.arc(point.x, this.axis_size - point.y * PRICE_SCALE, POINT_R, 0, Math.TAU, false);
+	    ctx.fill();
+	}
+    }
 
     click(e) {
-	console.log("Click on graph", e);
-	this.points.push({x: e.clientX - this.axis_offset,
-			  y: e.clientY - this.axis_offset});
+	//	this.points.push({x: e.clientX - this.axis_offset,
+//			  y: e.clientY - this.axis_offset});
 	this.draw();
     }
 };
